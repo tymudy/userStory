65c77ee8742ff9c1c4e58b2f7169701e8f6b1158
Emma Timea Sorban <emma.sorban@fortech.ro>
2017-03-29 18:02:05 +0300

    x

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -10,6 +10,7 @@
                         $scope.langs = ['en', 'pt'];
                         $scope.data = ""; 
                         $scope.newEstimation = {};  
+                        $scope.clickedestimation = {};
                  
                         $scope.gridOptions = {
                             enableSorting: false,
@@ -19,13 +20,13 @@
                                 { field: 'action', displayName: $translate.instant('action'), width: 180},
                                 { field: 'expectation', displayName: $translate.instant('expectation'), width: 300},
                                 { field: "sp", displayName: $translate.instant('sp'), width: 80},
-                                { field: "edit", displayName: $translate.instant('edit'), width: 80, cellTemplate:'<div class="grid-action-cell">'+'<button type="button" id="btn-resize" class="btn btn-info btn-xs" data-ng-click=edit()>Edit</button></div>'},
-                                { field: "delete", displayName: $translate.instant('delete'), cellTemplate:'<div class="grid-action-cell">'+'<button type="button" id="btn-resize" class="btn btn-danger btn-xs" data-ng-click=delete()>Delete</button></div>'}
+                                { field: "edit", displayName: $translate.instant('edit'), width: 80, cellTemplate:'<div class="grid-action-cell">'+'<button type="button" id="btn-resize" class="btn btn-info btn-xs" data-toggle="modal" data-target="#editModal" data-ng-click="edit(estimation)">Edit</button></div>'},
+                                { field: "delete", displayName: $translate.instant('delete'), cellTemplate:'<div class="grid-action-cell">'+'<button type="button" id="btn-resize" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#deleteModal" data-ng-click="delete(estimation)"">Delete</button></div>'}
                             ]
                         }
                         
                         $scope.gridHeader = $scope.gridOptions.columnDefs.slice(0,-2);
-                        
+                        $scope.gridData = $scope.gridOptions.columnDefs.data;
 
                         $scope.saveEstimation = function ( ) {
                              // $scope.gridOptions.data =[];
@@ -33,8 +34,8 @@
                              $scope.newEstimation = {}
                         }
 
-                        $scope.edit = function ( ) {
-                            
+                        $scope.edit = function ( estimation ) {
+                            console.log(estimation);
                         }
 
                         $scope.delete = function ( ) {
